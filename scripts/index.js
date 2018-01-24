9d01680b01e17a16ac716dd540f19271a733afbd
zakdrummond <zakdrummond@gmail.com>
2018-01-24 09:41:12 -0800

    add simplified store update

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
 
   api.getItems((items) => {
     items.forEach((item) => store.addItem(item));
+    const item = store.items[1];
+    console.log(`Current name: ${item.name}`);
+    store.findAndUpdate(item.id, { name: 'foobar'});
+    console.log(`New name: ${item.name}`);
     shoppingList.render();
   });
+
 });
